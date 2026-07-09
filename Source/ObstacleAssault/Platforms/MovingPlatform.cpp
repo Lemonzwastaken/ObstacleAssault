bf9347aab21df6593b2845efa5bce7f675986bfe
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-07-09 12:11:52 +0530

    Added all files to github
    Spent the past 2 hours setting up the project and now I will start tracking the progress as I create the game

---
+++ Source/ObstacleAssault/Platforms/MovingPlatform.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Platforms/MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+
+	Super::Tick(DeltaTime);
+	
+
+	//Im moving this platform by one unit in each direction
+	FVector CurrentLocation = GetActorLocation();
+
+	CurrentLocation.X = CurrentLocation.X + 1;
+
+	SetActorLocation(CurrentLocation);
+
+
+}
+
