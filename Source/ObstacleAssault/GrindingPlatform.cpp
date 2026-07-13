eb1ffe603d89325b5ed259ca1932c2662e5a3347
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-07-13 16:45:39 +0530

    Handles the case when an element in the GrindSplines array is reset via the editor

--- Source/ObstacleAssault/GrindingPlatform.cpp
+++ Source/ObstacleAssault/GrindingPlatform.cpp
@@ -4,27 +4,47 @@
 #include "GrindingPlatform.h"
 #include <Components/SplineComponent.h>
 
-#ifdef WITH_EDITOR
+#if WITH_EDITOR
 void AGrindingPlatform::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangeEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangeEvent);
 
+	// Handles the case when a new element is added to the GrindSplines array via the editor
 	if (PropertyChangeEvent.ChangeType == EPropertyChangeType::ArrayAdd)
 	{
 		const int32 NullElementIdx = GrindSplines.Find(nullptr);
 
 		if (NullElementIdx != INDEX_NONE)
 		{
-			TObjectPtr<USplineComponent> NewSplineComp = Cast<USplineComponent>(AddComponentByClass(USplineComponent::StaticClass(), false, FTransform{}, false));
+			TObjectPtr<USplineComponent> NewSplineComp = Cast<USplineComponent>(
+				AddComponentByClass(USplineComponent::StaticClass(), false, FTransform{}, false));
+
 			if (!NewSplineComp) return;
+
+			NewSplineComp->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 			AddInstanceComponent(NewSplineComp);
 			NewSplineComp->SetWorldScale3D(FVector{ 1.0 });
 			NewSplineComp->SetAbsolute(false, false, true);
 			GrindSplines[NullElementIdx] = NewSplineComp;
-
 		}
 	}
 
+	// Handles the case when an element in the GrindSplines array is reset via the editor
+	{
+		TArray<USplineComponent*> SplineCompsInOwnedComps{};
+		GetComponents(SplineCompsInOwnedComps);
 
+		for (USplineComponent* SplineComp : SplineCompsInOwnedComps)
+		{
+			if (!GrindSplines.Contains(SplineComp))
+			{
+				SplineComp->DestroyComponent();
+			}
+		}
+
+		GrindSplines.RemoveAll([](TObjectPtr<USplineComponent> SplineComp) {
+			return SplineComp == nullptr;
+			});
+	}
 }
-#endif //WITH EDITOR
+#endif // WITH_EDITOR
